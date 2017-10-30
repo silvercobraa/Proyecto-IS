a89946d443f7bb792de3fbb7ebb83b83f00b2223
silvercobraa <silver.cobra1n@gmail.com>
2017-10-30 14:33:49 -0300

    Dani ql

--- src/proyectois/ProyectoIS.java
+++ src/proyectois/ProyectoIS.java
@@ -15,7 +15,7 @@ public class ProyectoIS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("odiojava");
+        System.out.println("Dani ql");
     }
     
 }
